249418f80b82bf2b82990cb923de4e3128c60982
alnesterenko <alzpupkin@gmail.com>
2023-04-07 16:51:54 +0300

    6.5.3. Сортировка выборкой. [#156320]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -14,12 +14,6 @@ public class FindLoop {
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
         int rst = -1;
-        if (start < 0) {
-            start = 0;
-        }
-        if (finish > data.length - 1) {
-            finish = data.length - 1;
-        }
         for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rst = i;
---
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortSelectedTest {
+    @Test
+    public void whenSort() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenNeed3Sort() {
+        int[] data = new int[] {1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenNeed5Sort() {
+        int[] data = new int[] {1, 2, 5, 7, 0};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {0, 1, 2, 5, 7};
+        assertThat(result).containsExactly(expected);
+    }
+}
